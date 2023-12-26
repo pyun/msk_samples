806a05603bbe7523f51b72297b19e949d2a0ab83
pyun <yun.peng@aliyun.com>
2023-12-26 10:50:46 +0800

    Update main.go

--- go_msk/main.go
+++ go_msk/main.go
@@ -1,5 +1,5 @@
 /**
- * @Author: mcg
+ * @Author: pyun
  * @Date: 2021/2/25 23:01
  * @Desc:
  */
@@ -14,6 +14,7 @@ import (
 func main() {
 	iam := flag.Bool("iam", false, "是否采用iam角色认证，默认false")
 	flag.Parse()
-
-	service.Start(*iam)
+	if iam != nil {
+	  service.Start(*iam)
+	}
 }
